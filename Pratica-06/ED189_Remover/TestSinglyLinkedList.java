a82a5d64b715054a9db07a64479a9830ebe59146
Francisco Ribeiro <102924343+franciscoribeiro2003@users.noreply.github.com>
2022-11-15 15:33:55 +0000

    Update TestSinglyLinkedList.java

--- Pratica-06/ED189_Remover/TestSinglyLinkedList.java
+++ Pratica-06/ED189_Remover/TestSinglyLinkedList.java
@@ -1,11 +1,3 @@
-// -----------------------------------------------------------
-// Estruturas de Dados 2019/2020 (CC1007) - DCC/FCUP
-// http://www.dcc.fc.up.pt/~pribeiro/aulas/edados1920/
-// -----------------------------------------------------------
-// Exemplo de utilizacao da lista ligada simples
-// Ultima alteracao: 01/04/2018
-// -----------------------------------------------------------
-
 public class TestSinglyLinkedList {
     public static void main(String[] args) {
 
